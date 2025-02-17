6a13c87025600602ed651d4a167d1b9d83ac3564
lmj-990308 <gwanghaegun15@naver.com>
2025-02-17 14:12:25 +0900

    Add files via upload

--- streamlit_rag_memory_public.py
+++ streamlit_rag_memory_public.py
@@ -3,7 +3,7 @@ import streamlit as st
 import tempfile
 
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
