fd99e6cc2a192303907ebd63927ebd2b11c8275c
lmj-990308 <gwanghaegun15@naver.com>
2025-02-17 14:06:08 +0900

    Add files via upload

---
+++ streamlit_rag_memory_public.py
@@ -0,0 +1,82 @@
+import os
+import streamlit as st
+import tempfile
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+
+# OpenAI API 키 설정
+os.environ["OPENAI_API_KEY"] = st.secrets["OPENAI_API_KEY"]
+
+@st.cache_resource
+def load_and_split_pdf(file_path):
+    loader = PyPDFLoader(file_path)
+    return loader.load_and_split()
+
+@st.cache_resource
+def create_vector_store(_docs):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    split_docs = text_splitter.split_documents(_docs)
+    persist_directory = "./chroma_db"
+    vectorstore = Chroma.from_documents(
+        split_docs, 
+        OpenAIEmbeddings(model='text-embedding-3-small'),
+        persist_directory=persist_directory
+    )
+    return vectorstore
+
+@st.cache_resource
+def get_vectorstore(_docs):
+    persist_directory = "./chroma_db"
+    if os.path.exists(persist_directory):
+        return Chroma(
+            persist_directory=persist_directory,
+            embedding_function=OpenAIEmbeddings(model='text-embedding-3-small')
+        )
+    else:
+        return create_vector_store(_docs)
+
+st.header("헌법 Q&A 챗봇 💬 📚")
+option = st.selectbox("Select GPT Model", ("gpt-4o-mini", "gpt-3.5-turbo-0125"))
+
+uploaded_file = st.file_uploader("PDF 파일을 업로드해주세요", type="pdf")
+
+if uploaded_file is not None:
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as temp_file:
+        temp_file.write(uploaded_file.getvalue())
+        temp_path = temp_file.name
+
+    pages = load_and_split_pdf(temp_path)
+    vectorstore = get_vectorstore(pages)
+    retriever = vectorstore.as_retriever()
+
+    llm = ChatOpenAI(model=option)
+
+    def format_docs(docs):
+        return "\n\n".join(doc.page_content for doc in docs)
+
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", "다음 문서 기반으로 답변해 주세요."),
+            ("human", "{question}")
+        ]
+    )
+
+    rag_chain = (
+        {"context": retriever | format_docs, "question": RunnablePassthrough()}
+        | qa_prompt
+        | llm
+        | StrOutputParser()
+    )
+
+    question = st.text_input("질문을 입력해주세요")
+    if st.button("질문하기"):
+        if question:
+            with st.spinner("생각 중..."):
+                result = rag_chain.invoke({"question": question})
+                st.write(result)
